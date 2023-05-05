bad0f10e182d8d9ab77ae6682984c09d1ffb305b
faishal-developer <foysal.professional@gmail.com>
2023-05-06 05:42:13 +0600

    cycle detection topological_sorting graph_coloring

---
+++ Cycle_detection.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int Max = 1000;
+int inUsed[Max];
+vector<vector<int>> adjacent_list(Max);
+
+bool DFS(int src)
+{
+    // cout<<src<<" ";
+    inUsed[src] = 1;
+    for (int i = 0; i < adjacent_list[src].size(); i++)
+    {
+        if (inUsed[adjacent_list[src][i]] == 0)
+        {
+            bool value = DFS(adjacent_list[src][i]);
+            if (value == true)
+                return true;
+        }
+        if (inUsed[adjacent_list[src][i]] == 1)
+        {
+            return true;
+        }
+    }
+    inUsed[src] = 2;
+    return false;
+};
+
+int main()
+{
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adjacent_list[u].push_back(v);
+    }
+    bool isCycle = true;
+    for (int i = 1; i <= n; i++)
+    {
+        if (inUsed[i] == 0)
+        {
+            isCycle = DFS(i);
+            if (isCycle == true)
+            {
+                break;
+            }
+        }
+    }
+
+    isCycle ? cout << "Cycle exists" : cout << "no cycle";
+
+    return 0;
+}
+/**
+ * input
+4 4
+1 3
+2 1
+2 4
+3 4
+**/
--- topological_sorting.cpp
+++ topological_sorting.cpp
@@ -4,63 +4,69 @@ using namespace std;
 const int Max = 1000;
 int inUsed[Max];
 vector<vector<int>> adjacent_list(Max);
-stack<int>nodes;
+stack<int> nodes;
 
-
-bool DFS(int src){
+bool DFS(int src)
+{
     // cout<<src<<" ";
     inUsed[src] = 1;
     for (int i = 0; i < adjacent_list[src].size(); i++)
     {
-        if(inUsed[adjacent_list[src][i]]==0){
+        if (inUsed[adjacent_list[src][i]] == 0)
+        {
             bool value = DFS(adjacent_list[src][i]);
-            if(value==true) return true;
+            if (value == true)
+                return true;
         }
-        if (inUsed[adjacent_list[src][i]]==1){
+        if (inUsed[adjacent_list[src][i]] == 1)
+        {
             return true;
         }
     }
-    inUsed[src]=2;
+    inUsed[src] = 2;
     nodes.push(src);
     return false;
 };
 
-
 int main()
 {
-   
-    int n,m;
-    cin>>n>>m;
+
+    int n, m;
+    cin >> n >> m;
     for (int i = 0; i < m; i++)
     {
-        int u,v;
-        cin>>u>>v;
+        int u, v;
+        cin >> u >> v;
         adjacent_list[u].push_back(v);
     }
     bool isCycle = true;
     for (int i = 1; i <= n; i++)
     {
-        if(inUsed[i]==0){
-            isCycle  = DFS(i);
-            if(isCycle == true){
+        if (inUsed[i] == 0)
+        {
+            isCycle = DFS(i);
+            if (isCycle == true)
+            {
                 break;
             }
-        }   
+        }
     }
-    cout<<"problem solved"<<"\n";
-    
-    if(isCycle){
-        cout<<"IMPOSSIBLE";
-    }else{
+    cout << "problem solved"
+         << "\n";
+
+    if (isCycle)
+    {
+        cout << "IMPOSSIBLE";
+    }
+    else
+    {
         for (int i = 1; i <= n; i++)
         {
-            cout<<nodes.top()<<"  ";
+            cout << nodes.top() << "  ";
             nodes.pop();
         }
-        
     }
-    
-    
+
     return 0;
 }
 /**
