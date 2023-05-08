9273ff31ab2caee440ac5d28c4d2a8d5ab15b3da
faishal-developer <foysal.professional@gmail.com>
2023-05-08 10:59:01 +0600

    sssp for weighted graph

--- CPlus.cpp
+++ CPlus.cpp
@@ -1,72 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n,m;
 const int Max = 1000;
-int inUsed[Max];
-vector<vector<int>> adjacent_list(Max);
-stack<int>nodes;
+vector<int> isVisited(Max);
+vector<int> dist(Max);
 
-
-bool DFS(int src){
-    // cout<<src<<" ";
-    inUsed[src] = 1;
-    for (int i = 0; i < adjacent_list[src].size(); i++)
-    {
-        if(inUsed[adjacent_list[src][i]]==0){
-            bool value = DFS(adjacent_list[src][i]);
-            if(value==true) return true;
-        }
-        if (inUsed[adjacent_list[src][i]]==1){
-            return true;
-        }
-    }
-    inUsed[src]=2;
-    nodes.push(src);
-    return false;
-};
-
+void BFS_traversal(vector<vector<pair<int,int>>>nodes,int src){
+   queue<int> q;
+   q.push(src);
+   isVisited[src] = 1;
+   dist[src] = 0;
+   while (q.size())
+   {
+      int head = q.front();
+      vector<pair<int,int>> intermediate = nodes[head];
+      q.pop();
+      for (int i = 0; i < intermediate.size(); i++)
+      {
+         if (isVisited[intermediate[i].first]==0)
+         {
+            isVisited[intermediate[i].first]=1;
+            q.push(intermediate[i].first);
+            dist[intermediate[i].first] = dist[head]+intermediate[i].second;
+         }else{
+            int d = dist[intermediate[i].first];
+            if (d > dist[head] + intermediate[i].second){
+               dist[intermediate[i].first] = dist[head] + intermediate[i].second;
+            }
+         }
+      }
+      
+   }
+   
+   
+}
 
 int main()
 {
+   cin>>n>>m;
+   vector<vector<pair<int,int>>> SSSPNodes(n+1);
+
+   for (int i = 1; i <= m; i++)
+   {
+      int first, second,third;
+      cin >> first >> second>>third;
+      SSSPNodes[first].push_back({second,third});
+      SSSPNodes[second].push_back({first,third});
+   }
+   BFS_traversal(SSSPNodes,5);
+
+   for (int i = 1; i <=n; i++)
+   {
+      cout<<i<<"="<<dist[i]<<"\n";
+   }
    
-    int n,m;
-    cin>>n>>m;
-    for (int i = 0; i < m; i++)
-    {
-        int u,v;
-        cin>>u>>v;
-        adjacent_list[u].push_back(v);
-    }
-    bool isCycle = true;
-    for (int i = 1; i <= n; i++)
-    {
-        if(inUsed[i]==0){
-            isCycle  = DFS(i);
-            if(isCycle == true){
-                break;
-            }
-        }   
-    }
-    cout<<"problem solved"<<"\n";
-    
-    if(isCycle){
-        cout<<"IMPOSSIBLE";
-    }else{
-        for (int i = 1; i <= n; i++)
-        {
-            cout<<nodes.top()<<"  ";
-            nodes.pop();
-        }
-        
-    }
-    
-    
-    return 0;
+  
+   return 0;
 }
 /**
  * input
-5 3
-1 2
-3 1
-4 5
+9 14
+1 2 4
+1 8 8
+2 8 11
+2 3 8
+8 9 7
+8 7 1
+3 9 2
+3 4 7
+3 6 4
+9 7 6
+7 6 2
+4 5 9
+4 6 14
+6 5 10
 **/
---
+++ SSSP_BFS.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+const int Max = 1000;
+vector<int> isVisited(Max);
+vector<int> dist(Max);
+
+void BFS_traversal(vector<vector<pair<int, int>>> nodes, int src)
+{
+    queue<int> q;
+    q.push(src);
+    isVisited[src] = 1;
+    dist[src] = 0;
+    while (q.size())
+    {
+        int head = q.front();
+        vector<pair<int, int>> intermediate = nodes[head];
+        q.pop();
+        for (int i = 0; i < intermediate.size(); i++)
+        {
+            if (isVisited[intermediate[i].first] == 0)
+            {
+                isVisited[intermediate[i].first] = 1;
+                q.push(intermediate[i].first);
+                dist[intermediate[i].first] = dist[head] + intermediate[i].second;
+            }
+            else
+            {
+                int d = dist[intermediate[i].first];
+                if (d > dist[head] + intermediate[i].second)
+                {
+                    dist[intermediate[i].first] = dist[head] + intermediate[i].second;
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    vector<vector<pair<int, int>>> SSSPNodes(n + 1);
+
+    for (int i = 1; i <= m; i++)
+    {
+        int first, second, third;
+        cin >> first >> second >> third;
+        SSSPNodes[first].push_back({second, third});
+        SSSPNodes[second].push_back({first, third});
+    }
+    BFS_traversal(SSSPNodes, 5);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << "=" << dist[i] << "\n";
+    }
+
+    return 0;
+}
+/**
+ * input
+9 14
+1 2 4
+1 8 8
+2 8 11
+2 3 8
+8 9 7
+8 7 1
+3 9 2
+3 4 7
+3 6 4
+9 7 6
+7 6 2
+4 5 9
+4 6 14
+6 5 10
+**/
